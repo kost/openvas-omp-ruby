6da7bcb2007830de814d9482225f8073fd1f986f
Vlatko Kosturjak <kost@linux.hr>
2011-01-08 16:17:19 +0100

    Basic example directly in library source

--- lib/openvas-omp.rb
+++ lib/openvas-omp.rb
@@ -19,7 +19,21 @@
 #
 # == Usage:
 # 
-#  require 'openvas-omp'
+# require 'openvas-omp'
+#
+# ov=OpenVASOMP::OpenVASOMP.new("user"=>'openvas',"password"=>'openvas')
+# config=ov.config_get().index("Full and fast")
+# target=ov.target_create({"name"=>"t", "hosts"=>"127.0.0.1", "comment"=>"t"})
+# taskid=ov.task_create({"name"=>"t","comment"=>"t", "target"=>target, "config"=>config})
+# ov.task_start(taskid)
+# while not ov.task_finished(taskid) do
+#        stat=ov.task_get_byid(taskid)
+#        puts "Status: #{stat['status']}, Progress: #{stat['progress']} %"
+#        sleep 10
+# end
+# stat=ov.task_get_byid(taskid)
+# content=ov.report_get_byid(stat["lastreport"],'HTML')
+# File.open('report.html', 'w') {|f| f.write(content) }
 
 require 'socket' 
 require 'timeout'
